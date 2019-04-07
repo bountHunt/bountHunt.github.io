e86ba877f37deb3ff8b6712177873833f5f739b4
anvuong3 <anvuong3@github.com>
2019-04-06 19:54:25 -0700

    game

--- game.js
+++ game.js
@@ -17,8 +17,9 @@ function preload() {
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});
     this.load.image('background', 'assets/background.png');
+    this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});
+    
 }
 
 function create() {
