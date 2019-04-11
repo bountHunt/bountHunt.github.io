85cbaef505524380347cd08de9be3cf382390a82
Andrew Vuong <anvuong3@s.sfusd.edu>
2019-04-10 17:10:17 -0700

    hi

--- js/index.js
+++ js/index.js
@@ -1,108 +1,20 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 500,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+import Phaser from "phaser";
+import PlatformerScene from "./platformer-scene.js";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "game-container",
+  pixelArt: false,
+  backgroundColor: "#1d212d",
+  scene: PlatformerScene,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 1000 }
     }
+  }
 };
 
-var player;
-var platforms;
-var bplatforms;
-
-var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('roof', 'assets/bplatform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.image('background', 'assets/start_background.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-    this.load.spritesheet('bount', 'assets/main_right_walk.png', { frameWidth: 32, frameHeight: 48 });
-}
-
-function create ()
-{
-    this.add.image(400, 250, 'background').setScale(3.5);
-
-    platforms = this.physics.add.staticGroup();
-
-    bplatforms = this.physics.add.staticGroup();
-
-    platforms.create(400, 493.5, 'ground');
-
-    bplatforms.create(400, 593.5, 'roof').setScale(5).refreshBody();
-
-    player = this.physics.add.sprite(100, 250, 'dude').setScale(1);
-
-    player.setBounce(0);
-    player.setCollideWorldBounds(true);
-
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
-
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.physics.add.collider(player, platforms);
-
-    cursors = this.input.keyboard.createCursorKeys();
-    
-   
-
-
-}
-
-function update ()
-{
-    if (cursors.left.isDown)
-    {
-        player.setVelocityX(-160);
-
-        player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown)
-    {
-        player.setVelocityX(160);
-
-        player.anims.play('right', true);
-    }
-    else
-    {
-        player.setVelocityX(0);
-
-        player.anims.play('turn');
-    }
-
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.setVelocityY(-300);
-    }
-}
+const game = new Phaser.Game(config);
