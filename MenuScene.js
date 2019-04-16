f64abc2bbf5dd73395a3151350531ace8750fa4a
Andrew Vuong <anvuong3@s.sfusd.edu>
2019-04-15 17:59:45 -0700

    made scenes work YES

---
+++ MenuScene.js
@@ -0,0 +1,14 @@
+class MenuScene extends Phaser.Scene {
+    constructor() {
+        super({key:"MenuScene"});
+    }
+
+    preload() {
+        this.load.image('background', 'assets/background.jpg');
+
+    }
+
+    create() {
+        this.image = this.add.image(400,300, 'background')
+    }
+}
